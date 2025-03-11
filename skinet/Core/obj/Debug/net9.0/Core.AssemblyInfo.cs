e426b7f4ae014114737a723675e76c76b56df5e1
federico rogora <rogorafederico98@gmail.com>
2025-03-11 22:23:07 +0100

    end of section 2

--- skinet/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2abd3a38f7c8806b12f946c2b2a53076be5b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
